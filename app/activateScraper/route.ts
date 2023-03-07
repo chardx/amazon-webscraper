d34cb5d84fe6c67392f0bf8666881aa290e25a40
Richard Roxas <chardsourcex@gmail.com>
2023-03-07 16:20:37 +0800

    setting up activateScraper route

--- app/activateScraper/route.ts
+++ app/activateScraper/route.ts
@@ -1,7 +1,11 @@
-export async function POST(req: Request) {}
+import { NextApiRequest } from "next";
 
-export async function GET(req: Request) {
-  return new Response("Hello, Chad!", {
-    status: 200,
-  });
+export async function POST(req: NextApiRequest) {
+  const search = req.body.search;
 }
+
+// export async function GET(req: Request) {
+//   return new Response("Hello, Chad!", {
+//     status: 200,
+//   });
+// }
