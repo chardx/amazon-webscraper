61872195a5657832f22eb1934f694e886a9a11f4
Richard Roxas <chardsourcex@gmail.com>
2023-03-09 16:04:25 +0800

    added Results component

--- pages/api/activateScraper.ts
+++ pages/api/activateScraper.ts
@@ -1,4 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { adminDb } from "../../firebaseAdmin";
+import * as admin from "firebase-admin";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -14,29 +16,41 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
-  const { search } = req.body;
-  console.log("SEARCH IS >> ", search);
+  try {
+    const { search } = req.body;
+    console.log("SEARCH IS >> ", search);
 
-  const response = await fetch(
-    `https://api.brightdata.com/dca/trigger?collector=c_lexnax6y1ulihx7hgn&queue_next=1`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.BRIGHTDATA_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        search,
-      }),
-    }
-  );
+    const response = await fetch(
+      `https://api.brightdata.com/dca/trigger?collector=c_lexnax6y1ulihx7hgn&queue_next=1`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.BRIGHTDATA_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          search,
+        }),
+      }
+    );
 
-  const data = await response.json();
-  console.log("DATA IS >> ", data);
-  const { collection_id, start_eta } = data;
+    const data = await response.json();
+    console.log("DATA IS >> ", data);
+    const { collection_id, start_eta } = data;
 
-  res.status(200).json({
-    collection_id: "123",
-    start_eta: 123,
-  });
+    await adminDb.collection("searches").doc(collection_id).set({
+      search,
+      start_eta,
+      status: "pending",
+      updatedAt: start_eta,
+    });
+
+    res.status(200).json({
+      collection_id,
+      start_eta,
+    });
+  } catch (error: any) {
+    console.log("ERROR IS >> ", error);
+    return res.status(500).json({ error: error.message });
+  }
 }
