1cf4f3b05e3a58aa0196a3a1e8f26da184f4be46
Richard Roxas <chardsourcex@gmail.com>
2023-03-10 13:16:07 +0800

    code cleaning&quot

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -58,9 +58,7 @@ const SearchPage = ({ params: { id } }: Props) => {
         <div className="flex flex-col md:flex-row gap-x-4">
           <h1 className="font-bold">
             Search results for{" "}
-            <span className="text-indigo-600">
-              &quot{snapshot.data()?.search}&quot
-            </span>
+            <span className="text-indigo-600">{snapshot.data()?.search}</span>
           </h1>
           <p className="text-gray-300">
             {snapshot.data()?.results?.length > 0 &&
